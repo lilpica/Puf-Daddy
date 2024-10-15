3463b72d4c1d4abc2e8e8b5f1374ff26e516f7ef
lilpica <lilpica22@gmail.com>
2024-10-15 22:07:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -39,12 +39,12 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "v3.lavalink.rocks",
-        port: 443,
-        password: "horizxon.tech",
+        host: "lavalink.clxud.dev",
+        port: 2333,
+        password: "youshallnotpass",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
-        secure: true
+        secure: false
     },
  ]
 }
