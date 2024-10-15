29a17fa2b0cdab3f8a050e7b1ef1ed12b3d0f03c
lilpica <lilpica22@gmail.com>
2024-10-15 20:27:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "nodev4.fypmoon.org",
-        port: 1118,
-        password: "fypmoon.org",
+        host: "v4.lavalink.rocks",
+        port: 80,
+        password: "horizxon.tech",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
